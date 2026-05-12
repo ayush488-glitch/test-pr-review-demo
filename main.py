698a77537ff53094b692e51e5a6873fe42030694
Ayush Singh <ayush@test.com>
2026-05-13 03:05:47 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Simple user authentication module
+import hashlib
+
+def login(username, password):
+    # TODO: fix this later
+    if username == "admin" and password == "admin123":
+        return True
+    return False
+
+def get_user_data(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    return query
+
+def hash_password(password):
+    return hashlib.md5(password.encode()).hexdigest()
